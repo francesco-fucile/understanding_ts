1a4fc5edc8e05cc2c7463f1270a6464e1719663f
FrancescoFucile <francesc.fucile@ocyan.com>
2021-06-11 17:19:09 +0100

    update app.

--- app.ts
+++ app.ts
@@ -1,13 +1,16 @@
-// const person: {
-//     name: string;
-//     age: number;
-//     hobbies: 
-// } = {
-const person = {
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string];
+} = {
     name: 'Maximilian',
     age: 30,
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
 };
+
+person.role.push('admin');
 
 let favoriteActivities: string[];
 favoriteActivities = ['Sports'];
