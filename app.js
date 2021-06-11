b11345bf842c502c047aa22ed3774ab29ade8931
FrancescoFucile <francesc.fucile@ocyan.com>
2021-06-11 17:14:36 +0100

    update app.

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-function add(n1, n2, showResult, phrase) {
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return n1 + n2;
-    }
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: 
+// } = {
+var person = {
+    name: 'Maximilian',
+    age: 30,
+    hobbies: ['Sports', 'Cooking']
+};
+var favoriteActivities;
+favoriteActivities = ['Sports'];
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
 }
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = 'Result is: ';
-var result = add(number1, number2, printResult, resultPhrase);
--- app.ts
+++ app.ts
@@ -1,16 +1,19 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-    const result = n1 + n2
-    if (showResult) {
-        console.log(phrase + result);
-    } else {
-        return n1 + n2;
-    }
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: 
+// } = {
+const person = {
+    name: 'Maximilian',
+    age: 30,
+    hobbies: ['Sports', 'Cooking']
+};
+
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
+
+console.log(person.name)
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
 }
-
-let number1: number;
-number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = 'Result is: '
-
-const result = add(number1, number2, printResult, resultPhrase);
