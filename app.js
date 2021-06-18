158a60b607bcf82b02d7d4787ad94fab66651909
FrancescoFucile <francesc.fucile@ocyan.com>
2021-06-18 14:35:01 +0100

    function types.

--- app.js
+++ app.js
@@ -1,19 +1,11 @@
-function combine(input1, input2, result_conversion) {
-    var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    if (result_conversion === 'as-number') {
-        return +result;
-    }
-    else {
-        return result.toString();
-    }
+function add(n1, n2) {
+    return n1 + n2;
 }
-var combineAges = combine(30, 26, 'as-number');
-console.log(combineAges);
-var combinedNames = combine('Max', 'Anna', 'as-text');
-console.log(combinedNames);
+function printResult(num) {
+    console.log('Result: ' + num);
+}
+printResult(add(5, 12));
+var combineValues;
+combineValues = add;
+combineValues = printResult;
+console.log(combineValues(8, 8));
--- app.ts
+++ app.ts
@@ -1,26 +1,15 @@
-type Combinable = number | string;
-type ConversionDescription = 'as-number' | 'as-text'
-
-function combine(
-    input1: Combinable,
-    input2: Combinable,
-    result_conversion: ConversionDescription
-) {
-    let result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
-    } else {
-        result = input1.toString() + input2.toString();
-    }
-    if (result_conversion === 'as-number') {
-        return +result;
-    } else {
-        return result.toString();
-    }
+function add(n1: number, n2: number) {
+    return n1 + n2;
 }
 
-const combineAges = combine(30, 26, 'as-number');
-console.log(combineAges);
+ function printResult(num: number) {
+     console.log('Result: ' + num);
+ }
 
-const combinedNames = combine('Max', 'Anna', 'as-text');
-console.log(combinedNames);
+ printResult(add(5, 12));
+
+ let combineValues: (a: number, b: number) => number;
+
+ combineValues = add;
+
+ console.log(combineValues(8, 8));
